634da740ac714a3e4a5d1225592e98b12506df60
Shilpa Mali <malishilpa037@gmail.com>
2024-11-21 12:57:36 +0530

    Addition code

---
+++ Git/src/com/velocity/Example.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+public class Example {
+	public static void main(String[]args) {
+		
+		int a=20;
+		int b=30;
+		System.out.println("Hello Fellows");
+		int c=a+b;
+		System.out.println("Result is:"+c);
+		
+	}
+
+}
